70413f77e5e5963a868f132cef89fd62515a5307
Febby <gunawan.febby@gmail.com>
2020-10-28 16:45:30 +0700

    Update script.js
    - Added the set progress bar function to allow user click on the progress bar and update the progress accordingly.

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -102,15 +104,59 @@ function updateProgressBar(e){
     if (isPlaying){
     // console.log(e);
     const {duration, currentTime} = e.srcElement;
-    console.log(duration,currentTime);
+    // console.log(duration,currentTime);
     //update progress bar width
     const progresPercent = (currentTime / duration) * 100;
-    console.log(progresPercent);
-    progress.style.width = `${progresPercent}%`;       
+    // console.log(progresPercent);
+    progress.style.width = `${progresPercent}%`;
+    //calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    console.log('minutes', durationMinutes);       
+    //remainder
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+        durationSeconds = `0${durationSeconds}`;
+    }
+    console.log('seconds', durationSeconds);
+    //Delay switching duration element to avoid NaN
+    if (durationSeconds) {
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+
+    //calculate display for current
+    const currentMinutes = Math.floor(currentTime / 60);
+    // console.log('minutes', currentMinutes);       
+    //remainder
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+        currentSeconds = `0${currentSeconds}`;
+    }
+    // console.log('seconds', currentSeconds);
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
-// Event
+
+// Set Progress Bar
+
+function setProgressBar(e) {
+    console.log(e);
+    const width = this.clientWidth;
+    console.log('width', width);
+    const clickX = e.offsetX;
+    console.log('clickX', clickX);
+    //destructe
+    const { duration } = music;
+    console.log(clickX / width);
+    console.log(clickX / width * duration);
+    music.currentTime = (clickX / width) * duration;
+}
+
+
+
+// Event Listener
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
